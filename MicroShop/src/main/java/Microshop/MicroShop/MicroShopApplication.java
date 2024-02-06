59c23014890b6b07add3df3795341f3b1db723a3
vineshreddy845 <64718480+vineshreddy845@users.noreply.github.com>
2024-02-05 23:43:46 -0600

    Initial commit

---
+++ MicroShop/src/main/java/Microshop/MicroShop/MicroShopApplication.java
@@ -0,0 +1,13 @@
+package Microshop.MicroShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroShopApplication.class, args);
+	}
+
+}
---
+++ MicroShop/src/test/java/Microshop/MicroShop/MicroShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Microshop.MicroShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
